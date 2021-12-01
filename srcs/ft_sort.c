a4ea5c343e02a0e62bea54ec0b0e80145859f240
Gaetan SAP <gsap@e2r5p10.clusters.42paris.fr>
2021-12-01 15:13:09 +0100

    0_leaks

--- srcs/ft_sort.c
+++ srcs/ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: gsap <gsap@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 14:13:18 by gsap              #+#    #+#             */
-/*   Updated: 2021/12/01 10:39:42 by gsap             ###   ########.fr       */
+/*   Updated: 2021/12/01 15:12:30 by gsap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ t_swap	ft_sort_stack(t_swap stack)
 	if (ft_lstrlen(stack.a) == 100)
 		stack = ft_sort_chunk(stack, tmp);
 	else if (ft_lstrlen(stack.a) < 100)
+	{
 		stack = ft_basic_sort_a(stack);
+		ft_free_ls(tmp);
+	}
 	else
 		stack = ft_radix_sort(stack, tmp);
 	return (stack);
